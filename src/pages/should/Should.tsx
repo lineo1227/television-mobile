8316d157c3312d99c14a8b17b52dff69d9fd7e01
lineo <1445154800@qq.com>
2024-06-11 18:54:14 +0800

    feat: 求片ui完成

--- src/pages/should/Should.tsx
+++ src/pages/should/Should.tsx
@@ -1,3 +1,24 @@
+import LHeader from "@/components/header/LHeader";
+import "./should.scss";
+import { TextArea } from "antd-mobile";
+import { useState } from "react";
 export default function Should() {
-    return <div>Should</div>;
+    const [value, setValue] = useState("");
+    return (
+        <div className="should">
+            <LHeader link={true} back={true} />
+            <div className="should-container">
+                <h2>把想看的影视留言</h2>
+                <p>xx影视，给你最好的体验！</p>
+                <TextArea
+                    placeholder="请输入内容"
+                    value={value}
+                    onChange={(val) => {
+                        setValue(val);
+                    }}
+                />
+                <button>点击提交</button>
+            </div>
+        </div>
+    );
 }
